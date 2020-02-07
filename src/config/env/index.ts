be0fb9c97a1149a93f4bf4cd1c8a908d320bbf79
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2020-02-07 09:11:10 +0200

    Added CONFIG environment variable

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,15 +1,17 @@
-import * as _                from "lodash";
-import { commonConfig }      from "./common";
+import * as _ from "lodash";
+import { commonConfig } from "./common";
 import { developmentConfig } from "./development";
-import { productionConfig }  from "./production";
+import { productionConfig } from "./production";
 
 export function getConfig() {
 
+    const config = process.env.CONFIG ? JSON.parse(process.env.CONFIG) : {};
+
     switch (process.env.NODE_ENV) {
         case "development":
-            return _.merge(commonConfig, developmentConfig);
+            return _.merge(commonConfig, developmentConfig, config);
         case "production":
-            return _.merge(commonConfig, productionConfig);
+            return _.merge(commonConfig, productionConfig, config);
         default:
             return commonConfig;
     }
