be524e4fd7a9b5882bd6e07cb80e23ed688f4a97
RaduCStefanescu <stefanescuradu@gmail.com>
2020-02-25 11:34:06 +0200

    Add GHA pipeline on pull request

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# TODO: REMOVE as this prints the secrets
-env
-
 envsubst _ormconfig.json > ormconfig.json
-
-# TODO: REMOVE as this prints the secrets
-cat ormconfig.json
 
 npm run serve
